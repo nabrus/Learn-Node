56fa64400a53a3a88280767d6dcbdac25360eeb4
towelie <34988577+nabrus@users.noreply.github.com>
2019-11-16 22:52:23 -0500

    clean up code

--- dang_thats_delicious/data/load-sample-data.js
+++ dang_thats_delicious/data/load-sample-data.js
@@ -32,7 +32,7 @@ async function loadData() {
     await User.insertMany(users);
     console.log("👍👍👍👍👍👍👍👍 Done!");
     process.exit();
-  } catch(e) {
+  } catch (e) {
     console.log("\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n");
     console.log(e);
     process.exit();
