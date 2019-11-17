e2fedb20a950fe0e4e7ff09665231466a45fdccd
towelie <34988577+nabrus@users.noreply.github.com>
2019-11-17 10:28:23 -0500

    cleanup comment

--- dang_thats_delicious/helpers.js
+++ dang_thats_delicious/helpers.js
@@ -5,7 +5,8 @@
 // FS is a built in module to node that let's us read files from the system we're running on
 const fs = require("fs");
 
-// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
+// moment.js is a handy library for displaying dates. We need this in our
+// templates to display things like "Posted 5 minutes ago"
 exports.moment = require("moment");
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
